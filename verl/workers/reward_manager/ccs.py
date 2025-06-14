ade9c07eeaaef07a507f09347f14aca20a704bde
YinongLong <15116330197@163.com>
2025-06-14 20:19:05 +0800

    update custom reward manager

--- verl/workers/reward_manager/ccs.py
+++ verl/workers/reward_manager/ccs.py
@@ -11,115 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import collections
 import json
-import re
-import requests
 from collections import defaultdict
-from concurrent import futures
-from requests.exceptions import Timeout, RequestException
 from typing import Callable, Optional
 
 import torch
+from doraemon.formatters import consistency
+from doraemon.formatters.utils import extract_think_answer
+from doraemon.inference import service
 from transformers import PreTrainedTokenizer
 
 from verl import DataProto
 from verl.utils.reward_score import default_compute_score
-
-
-def request_remote_service(service_url, data_items, timeout=5.0):
-    """
-    请求部署的一致性判别模型，判断采样的思维链和json结果之间的关系
-    """
-    ans = []
-    for i, session, cot, se_dict in data_items:
-        try:
-            response = requests.post(
-                url=service_url,
-                json={
-                    'session': json.dumps(session, ensure_ascii=False),
-                    'cot': cot,
-                    'se_dict_str': json.dumps(se_dict, ensure_ascii=False)
-                },
-                timeout=timeout
-            )
-            re_dict = response.json()
-            ans.append((i, re_dict['cons_flag']))
-        except (Timeout, RequestException):
-            ans.append((i, '一致'))
-    return ans
-
-
-def concurrent_requests(container, service_url, num_threads):
-    """
-    并发请求远程服务
-    """
-    num_items = len(container)
-    num_base = num_items // num_threads
-    num_resi = num_items % num_threads
-
-    def get_bs():
-        nonlocal num_resi
-        bs = num_base
-        if num_resi:
-            bs += 1
-            num_resi -= 1
-        return bs
-
-    ans = []
-    with futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures_arr = []
-
-        capacity = []
-        bs = get_bs()
-        for i, (session, cot, se_dict) in enumerate(container):
-            capacity.append((i, session, cot, se_dict))
-            if len(capacity) < bs:
-                continue
-            future = executor.submit(
-                request_remote_service, service_url, capacity
-            )
-            futures_arr.append(future)
-            capacity = []
-            bs = get_bs()
-        if capacity:
-            future = executor.submit(
-                request_remote_service, service_url, capacity
-            )
-            futures_arr.append(future)
-            capacity = []
-
-        for future in futures.as_completed(futures_arr):
-            part_ans = future.result()
-            ans.extend(part_ans)
-    ans.sort(key=lambda item: item[0])
-    ans = [item[-1] for item in ans]
-    return ans
-
-
-def extract_think_answer(solution_str):
-    """
-    按照预期抽取模型回复中的推理部分和答案结果部分
-    """
-    pattern = r'^\s*<analysis>(.+?)</analysis>\s*<answer>(.+?)</answer>\s*$'
-    se_ans = re.search(pattern, solution_str, re.DOTALL)
-    if se_ans is None:
-        return None, None
-
-    think_part = se_ans.group(1)
-    answer_part = se_ans.group(2)
-    if re.findall(r'<analysis>|</analysis>|<answer>|</answer>', think_part):
-        return None, None
-
-    if re.findall(r'<analysis>|</analysis>|<answer>|</answer>', answer_part):
-        return None, None
-
-    think_part = think_part.strip()
-    answer_part = answer_part.strip()
-    if not think_part or not answer_part:
-        return None, None
-
-    return think_part, answer_part
+from verl.workers.reward_manager import register
 
 
 def get_se_dict(answer_str):
@@ -140,9 +45,10 @@ def get_se_dict(answer_str):
     return answer_json
 
 
+@register("ccs")
 class CCSRewardManager:
     """
-    针对中控强化学习的奖励定制化处理
+    customize our own reward manager
     """
 
     def __init__(
@@ -151,26 +57,41 @@ class CCSRewardManager:
         num_examine: int,
         compute_score: Optional[Callable] = None,
         reward_fn_key: str = 'data_source',
-        service_url: str = 'http://172.16.0.2:60025/consistency/qwen3_4b',
-        num_threads: int = 128
+        tokenizer_dir: Optional[str] = None,
+        service_conf: Optional[str] = None
     ) -> None:
         self.tokenizer = tokenizer
         self.num_examine = num_examine  # the number of batches of decoded responses to print to the console
         self.compute_score = compute_score or default_compute_score
         self.reward_fn_key = reward_fn_key
-        self.service_url = service_url
-        self.num_threads = num_threads
+        self.tokenizer_dir = tokenizer_dir
+        self.service_conf = service_conf
 
-    def request_service(self, container):
+        self.data_formatter = consistency.DataFormatter(tokenizer_dir)
+
+    def request_service(self, container, n=1, temperature=0.0):
         """
         请求远程服务，判断CoT和json结果的一致性
         """
-        return concurrent_requests(container, self.service_url, self.num_threads)
+        completions = service.get_completions(container, self.service_conf, n=n, temperature=temperature)
+        flags = []
+        for com_item in completions:
+            counter = collections.Counter()
+            for choice in com_item.choices:
+                if choice.finish_reason != 'stop':
+                    continue
+                flag = choice.text
+                if flag not in {'一致', '不一致'}:
+                    continue
+                counter[flag] += 1
+            if not counter:
+                flag = '一致'
+            else:
+                flag = counter.most_common()[0][0]
+            flags.append(flag)
+        return flags
 
     def judge(self, data):
-        """
-        judge the consistency between CoT and answer
-        """
         res = []
         container = []
         for i in range(len(data)):
@@ -187,13 +108,14 @@ class CCSRewardManager:
             cot, se_dict_str = extract_think_answer(response_str)
             se_dict = get_se_dict(se_dict_str)
             if cot is None or se_dict is None:
-                res.append((cot, se_dict, -1))  # -1 代表推理结果格式错误，不需要判断一致性
+                res.append((cot, se_dict, -1))  # -1 represents error
             else:
                 res.append((cot, se_dict, len(container)))
                 extra_info = data_item.non_tensor_batch.get('extra_info', None)
                 assert extra_info is not None
                 session = json.loads(extra_info['session'])
-                container.append((session, cot, se_dict))
+                prompt = self.data_formatter.format(session, cot, se_dict)
+                container.append(prompt)
         flags = self.request_service(container)
         return res, flags
 
@@ -209,7 +131,6 @@ class CCSRewardManager:
             else:
                 return data.batch['rm_scores']
 
-        # 请求远程服务，判断模型推理思维链和json结果的一致性关系
         ext_res, consist_flags = self.judge(data)
         
         reward_tensor = torch.zeros_like(data.batch["responses"], dtype=torch.float32)
@@ -276,6 +197,7 @@ class CCSRewardManager:
                         print(f"[{key}]", value)
                 else:
                     print("[score]", score)
+                print("[consistency]", c_flag)
 
         if return_dict:
             return {
